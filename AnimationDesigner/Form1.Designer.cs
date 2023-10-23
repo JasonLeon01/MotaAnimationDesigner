afa18a5a6434774c479a8e54aaacaec4ca6ba839
Jason Leon <jasonleon01@outlook.com>
2023-10-24 00:19:04 +0800

    update

--- AnimationDesigner/Form1.Designer.cs
+++ AnimationDesigner/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             listBox1 = new ListBox();
             label1 = new Label();
             textBox1 = new TextBox();
@@ -45,6 +46,9 @@
             label6 = new Label();
             textBox2 = new TextBox();
             label7 = new Label();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
@@ -54,8 +58,9 @@
             listBox1.ItemHeight = 17;
             listBox1.Location = new Point(1, 38);
             listBox1.Name = "listBox1";
-            listBox1.Size = new Size(85, 361);
+            listBox1.Size = new Size(85, 327);
             listBox1.TabIndex = 0;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
             // 
             // label1
             // 
@@ -73,6 +78,7 @@
             textBox1.Size = new Size(120, 23);
             textBox1.TabIndex = 2;
             textBox1.Text = "无";
+            textBox1.TextChanged += textBox1_TextChanged;
             // 
             // label2
             // 
@@ -98,52 +104,58 @@
             listBox2.ItemHeight = 17;
             listBox2.Location = new Point(102, 72);
             listBox2.Name = "listBox2";
-            listBox2.Size = new Size(67, 293);
+            listBox2.Size = new Size(107, 293);
             listBox2.TabIndex = 5;
+            listBox2.SelectedIndexChanged += listBox2_SelectedIndexChanged;
             // 
             // button1
             // 
             button1.Location = new Point(99, 372);
             button1.Name = "button1";
-            button1.Size = new Size(70, 27);
+            button1.Size = new Size(110, 27);
             button1.TabIndex = 6;
             button1.Text = "新建帧";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // button2
             // 
-            button2.Location = new Point(1, 405);
+            button2.Location = new Point(225, 378);
             button2.Name = "button2";
-            button2.Size = new Size(85, 33);
+            button2.Size = new Size(300, 36);
             button2.TabIndex = 7;
-            button2.Text = "全部保存";
+            button2.Text = "保存动画数据";
             button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // pictureBox1
             // 
-            pictureBox1.Location = new Point(191, 72);
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(225, 72);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(293, 293);
+            pictureBox1.Size = new Size(300, 300);
             pictureBox1.TabIndex = 8;
             pictureBox1.TabStop = false;
             // 
             // button3
             // 
-            button3.Location = new Point(490, 72);
+            button3.Location = new Point(540, 72);
             button3.Name = "button3";
             button3.Size = new Size(67, 82);
             button3.TabIndex = 9;
             button3.Text = "设置当前帧动画";
             button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
             // 
             // button4
             // 
-            button4.Location = new Point(490, 282);
+            button4.Location = new Point(540, 289);
             button4.Name = "button4";
             button4.Size = new Size(67, 83);
             button4.TabIndex = 10;
             button4.Text = "播放动画";
             button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
             // 
             // label4
             // 
@@ -165,12 +177,13 @@
             // 
             // button5
             // 
-            button5.Location = new Point(416, 12);
+            button5.Location = new Point(468, 10);
             button5.Name = "button5";
             button5.Size = new Size(30, 21);
             button5.TabIndex = 13;
             button5.Text = "...";
             button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
             // 
             // label6
             // 
@@ -188,6 +201,7 @@
             textBox2.Size = new Size(35, 23);
             textBox2.TabIndex = 15;
             textBox2.Text = "0";
+            textBox2.TextChanged += textBox2_TextChanged;
             // 
             // label7
             // 
@@ -198,11 +212,44 @@
             label7.TabIndex = 16;
             label7.Text = "帧";
             // 
+            // button6
+            // 
+            button6.Location = new Point(1, 372);
+            button6.Name = "button6";
+            button6.Size = new Size(85, 27);
+            button6.TabIndex = 17;
+            button6.Text = "新建空动画";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(99, 405);
+            button7.Name = "button7";
+            button7.Size = new Size(110, 27);
+            button7.TabIndex = 18;
+            button7.Text = "删除帧";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(1, 405);
+            button8.Name = "button8";
+            button8.Size = new Size(85, 27);
+            button8.TabIndex = 19;
+            button8.Text = "删除该动画";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(96F, 96F);
             AutoScaleMode = AutoScaleMode.Dpi;
-            ClientSize = new Size(573, 450);
+            ClientSize = new Size(620, 450);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
             Controls.Add(label7);
             Controls.Add(textBox2);
             Controls.Add(label6);
@@ -221,10 +268,12 @@
             Controls.Add(label1);
             Controls.Add(listBox1);
             FormBorderStyle = FormBorderStyle.FixedDialog;
+            Icon = (Icon)resources.GetObject("$this.Icon");
             MaximizeBox = false;
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "动画编辑器";
+            Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
@@ -249,5 +298,8 @@
         private Label label6;
         private TextBox textBox2;
         private Label label7;
+        private Button button6;
+        private Button button7;
+        private Button button8;
     }
 }
--- AnimationDesigner/Form1.cs
+++ AnimationDesigner/Form1.cs
@@ -1,20 +1,263 @@
+using System;
+using System.Media;
 using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
 
 namespace AnimationDesigner
 {
     public partial class Form1 : Form
     {
-        int playTime;
-        List<List<Animation>> anime;
+        int list1Index, list2Index;
+        List<Animation> anime;
+        Image back;
         public Form1()
         {
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-            playTime = 0;
-            anime = new List<List<Animation>>();
+            list1Index = 0;
+            list2Index = 0;
+            anime = new List<Animation>();
             InitializeComponent();
+            back = pictureBox1.Image;
             this.SetStyle(ControlStyles.OptimizedDoubleBuffer | ControlStyles.ResizeRedraw | ControlStyles.AllPaintingInWmPaint, true);
             this.SetStyle(ControlStyles.Opaque, false);
             this.UpdateStyles();
+        }
+        private void loadAnimation()
+        {
+            int idx = 0;
+            string path = Application.StartupPath + @"data\animation\";
+            while (File.Exists(path + @"animation_" + idx.ToString() + ".dat"))
+            {
+                string file = @"animation_" + idx.ToString() + ".dat";
+                string datatext = System.IO.File.ReadAllText(path + file);
+                string[] data = datatext.Split(Environment.NewLine.ToCharArray());
+                data = data.Where(s => !string.IsNullOrEmpty(s)).ToArray();
+                listBox1.Items.Add(idx.ToString().PadLeft(3, '0') + "：" + data[1].Split(':')[1]);
+                anime.Add(new Animation(data[1].Split(':')[1], new List<string>(data[2].Split(':')[1].Split(',')), data[3].Split(':')[1], int.Parse(data[4].Split(':')[1])));
+                ++idx;
+            }
+            if (anime.Count == 0)
+            {
+                MessageBox.Show("未检测到动画数据！");
+                Application.Exit();
+                return;
+            }
+        }
+        private void drawAnimation(int nowid)
+        {
+            if (anime[listBox1.SelectedIndex].animationPatterns.Count > 0 && listBox2.SelectedIndex != -1 && nowid != -1 && anime[listBox1.SelectedIndex].animationPatterns[nowid] != "blank")
+            {
+                Bitmap buffer = new Bitmap(300, 300);
+                Graphics g = Graphics.FromImage(buffer);
+                Image img = Image.FromFile(Application.StartupPath + @"graphics\animation\" + anime[listBox1.SelectedIndex].animationPatterns[nowid]);
+                g.DrawImage(back, new Rectangle(0, 0, 300, 300));
+                g.DrawImage(img, new Rectangle((300 - img.Width) / 2, (300 - img.Height) / 2, img.Width, img.Height));
+                pictureBox1.Image = buffer;
+                g.Dispose();
+            }
+            else
+                pictureBox1.Image = back;
+        }
+        private void refreshList1()
+        {
+            listBox1.Items.Clear();
+            int idx = 0;
+            foreach (Animation ani in anime)
+            {
+                listBox1.Items.Add(idx.ToString().PadLeft(3, '0') + "：" + ani.animationName);
+                ++idx;
+            }
+            listBox1.SelectedIndex = list1Index;
+        }
+        private void refreshList2()
+        {
+            listBox2.Items.Clear();
+            foreach (string str in anime[listBox1.SelectedIndex].animationPatterns)
+                listBox2.Items.Add(str);
+            if (anime[listBox1.SelectedIndex].animationPatterns.Count > 0)
+                listBox2.SelectedIndex = list2Index;
+            else
+                listBox2.ClearSelected();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadAnimation();
+            listBox1.SelectedIndex = 0;
+            textBox1.Text = anime[0].animationName;
+            label5.Text = anime[0].SEFile;
+            label5.Refresh();
+            textBox2.Text = anime[0].SETime.ToString();
+            listBox2.Items.Clear();
+            foreach (string str in anime[0].animationPatterns)
+                listBox2.Items.Add(str);
+
+            if (anime[0].animationPatterns.Count > 0)
+            {
+                listBox2.SelectedIndex = 0;
+                Bitmap buffer = new Bitmap(300, 300);
+                Graphics g = Graphics.FromImage(buffer);
+                Image img = Image.FromFile(Application.StartupPath + @"graphics\animation\" + anime[0].animationPatterns[0]);
+                g.DrawImage(back, new Rectangle(0, 0, 300, 300));
+                g.DrawImage(img, new Rectangle((300 - img.Width) / 2, (300 - img.Height) / 2, img.Width, img.Height));
+                pictureBox1.Image = buffer;
+                g.Dispose();
+            }
+            else
+            {
+                listBox2.ClearSelected();
+                pictureBox1.BackgroundImage = back;
+            }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            anime[listBox1.SelectedIndex].animationName = textBox1.Text;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            anime[listBox1.SelectedIndex].SETime = int.Parse(textBox2.Text);
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            list1Index = listBox1.SelectedIndex;
+            textBox1.Text = anime[listBox1.SelectedIndex].animationName;
+            label5.Text = anime[listBox1.SelectedIndex].SEFile;
+            label5.Refresh();
+            textBox2.Text = anime[listBox1.SelectedIndex].SETime.ToString();
+            listBox2.Items.Clear();
+            foreach (string str in anime[listBox1.SelectedIndex].animationPatterns)
+                listBox2.Items.Add(str);
+            if (anime[listBox1.SelectedIndex].animationPatterns.Count > 0)
+                listBox2.SelectedIndex = 0;
+            else
+                listBox2.ClearSelected();
+            drawAnimation(listBox2.SelectedIndex);
+        }
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            list2Index = listBox2.SelectedIndex;
+            drawAnimation(listBox2.SelectedIndex);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //创建对象
+            OpenFileDialog ofg = new OpenFileDialog();
+            //设置默认打开路径
+            ofg.InitialDirectory = Application.StartupPath + @"sound";
+            //设置打开标题、后缀
+            ofg.Title = "请选择导入音频文件";
+            ofg.Filter = "wav文件|*.wav|ogg文件|*.ogg|vorbis文件|*.vorbis|flac文件|*.flac";
+            string path = "";
+            DialogResult s = ofg.ShowDialog();
+            if (s == DialogResult.OK)
+            {
+                //得到打开的文件路径（包括文件名）
+                string[] names = ofg.FileName.ToString().Split('\\');
+                label5.Text = names[names.Length - 1];
+                label5.Refresh();
+                anime[listBox1.SelectedIndex].SEFile = label5.Text;
+            }
+            else if (s == DialogResult.Cancel)
+                MessageBox.Show("未选择打开文件！");
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            anime[listBox1.SelectedIndex].animationPatterns.Insert(listBox2.SelectedIndex + 1, "blank");
+            refreshList2();
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (anime[listBox1.SelectedIndex].animationPatterns.Count == 0)
+            {
+                MessageBox.Show("已经没有可以删除的啦！");
+                return;
+            }
+            anime[listBox1.SelectedIndex].animationPatterns.RemoveAt(listBox2.SelectedIndex);
+            if (list2Index >= anime[listBox1.SelectedIndex].animationPatterns.Count) list2Index = anime[listBox1.SelectedIndex].animationPatterns.Count - 1;
+            refreshList2();
+            drawAnimation(listBox2.SelectedIndex);
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            anime.Add(new Animation("undefined", new List<string>(), "undefined", 0));
+            refreshList1();
+        }
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (anime.Count == 1)
+            {
+                MessageBox.Show("不允许删除最后剩余的动画！");
+                return;
+            }
+            anime.RemoveAt(listBox1.SelectedIndex);
+            if (list1Index >= anime.Count) list1Index = anime.Count - 1;
+            refreshList1();
+            refreshList2();
+            drawAnimation(listBox2.SelectedIndex);
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //创建对象
+            OpenFileDialog ofg = new OpenFileDialog();
+            //设置默认打开路径
+            ofg.InitialDirectory = Application.StartupPath + @"graphics\animation";
+            //设置打开标题、后缀
+            ofg.Title = "请选择导入动画文件";
+            ofg.Filter = "png文件|*.png";
+            string path = "";
+            DialogResult s = ofg.ShowDialog();
+            if (s == DialogResult.OK)
+            {
+                //得到打开的文件路径（包括文件名）
+                string[] names = ofg.FileName.ToString().Split('\\');
+                anime[listBox1.SelectedIndex].animationPatterns[listBox2.SelectedIndex] = names[names.Length - 1];
+                refreshList2();
+                drawAnimation(listBox2.SelectedIndex);
+            }
+            else if (s == DialogResult.Cancel)
+                MessageBox.Show("未选择打开文件！");
+        }
+        private async void button4_Click(object sender, EventArgs e)
+        {
+            bool banned = false;
+            if (anime[listBox1.SelectedIndex].SEFile.Split('.')[1] == "ogg")
+            {
+                banned = true;
+                MessageBox.Show("抱歉，C#并不支持ogg文件的播放，本次播放只能静音");
+            }
+            for (int i = 0; i < anime[listBox1.SelectedIndex].animationPatterns.Count; ++i)
+            {
+                drawAnimation(i);
+                if (i == anime[listBox1.SelectedIndex].SETime && !banned)
+                {
+                    var audioFile = new AudioFileReader(Application.StartupPath + @"sound/" + anime[listBox1.SelectedIndex].SEFile);
+                    var outputDevice = new WaveOutEvent();
+                    outputDevice.Init(audioFile);
+                    outputDevice.Play();
+                }
+                await Task.Delay(64);
+            }
+            drawAnimation(listBox2.SelectedIndex);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int idx = 0;
+            string file = Application.StartupPath + @"data\animation\";
+            foreach (Animation ani in anime)
+            {
+                string data = "[animation]\n";
+                data = data + "name:" + ani.animationName + "\n";
+                data = data + "pattern:" + string.Join(",", ani.animationPatterns) + "\n";
+                data = data + "SEFile:" + ani.SEFile + "\n";
+                data = data + "SETime:" + ani.SETime.ToString() + "\n";
+                System.IO.File.WriteAllText(file + @"animation_" + idx.ToString() + ".dat", data);
+                idx++;
+            }
+            MessageBox.Show("保存成功！");
+            return;
         }
     }
 }
@@ -22,7 +265,7 @@ namespace AnimationDesigner
 class Animation
 {
     public string animationName, SEFile;
-    public List <string> animationPatterns;
+    public List<string> animationPatterns;
     public int SETime;
     public Animation()
     {
