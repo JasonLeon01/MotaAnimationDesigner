71cb4a2ae2b07dbd4c81ae1356fdec880ecde3cd
Jason Leon <jasonleon01@outlook.com>
2023-11-03 20:21:25 +0800

    update

--- AnimationDesigner/Form1.cs
+++ AnimationDesigner/Form1.cs
@@ -197,7 +197,7 @@ namespace AnimationDesigner
             //创建对象
             OpenFileDialog ofg = new OpenFileDialog();
             //设置默认打开路径
-            ofg.InitialDirectory = Application.StartupPath + @"..\graphics\animation";
+            ofg.InitialDirectory = Path.GetDirectoryName(Path.GetDirectoryName(Application.ExecutablePath)) + @"\graphics\animation";
             //设置打开标题、后缀
             ofg.Title = "请选择导入动画文件";
             ofg.Filter = "png文件|*.png";
